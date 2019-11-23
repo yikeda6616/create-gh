b9b5d3e54c80a7b5a2399ec7a30bd998097a3230
Yasushi Ikeda <yikeda6616@gmail.com>
2019-11-23 12:28:02 +0900

    Basic scaffolding

---
+++ github.ts
@@ -0,0 +1,6 @@
+import puppeteer from 'puppeteer';
+
+const repositoryName = process.argv[2];
+console.log(`Repository Title: ${repositoryName}`);
+
+export default github;
---
+++ index.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+
+import gh from './github';
+
+const env = { ...(dotenv.config().parsed as any) };
+
+(async () => {
+  await gh.initialize();
+
+  await gh.login(env.USERNAME, env.PASSWORD);
+
+  await gh.photoSaveProcßessß(['audi', 'bmw']);
+
+  await gh.close();
+})();
